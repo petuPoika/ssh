bb0da67a5851821a651bed61bb8c2cb14a17026f
Petteri Castren <petu.castren@hotmail.com>
2024-10-29 18:24:03 +0200

    US5 - Test 2

--- test/testspreadsheet.py
+++ test/testspreadsheet.py
@@ -62,3 +62,8 @@ class TestSpreadSheet(TestCase):
         spreadsheet = SpreadSheet()
         spreadsheet.set("A1", "=1+3")
         self.assertEqual(4, spreadsheet.evaluate("A1"))
+
+    def test_evaluate_invalid_formula_sum(self):
+        spreadsheet = SpreadSheet()
+        spreadsheet.set("A1", "=1+3.5")
+        self.assertEqual("#Error", spreadsheet.evaluate("A1"))
